0ba2f882b73aa203d75d67865ede2ca446e75eb5
KanimozhiSivakumar <118879210+KanimozhiSivakumar@users.noreply.github.com>
2022-11-25 23:58:59 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+
+var arr = [
+    {
+        "name" : "vivien",
+        "age" : "4"
+    },
+
+    {
+        "name" : "siva",
+        "age" : "33"
+    },
+    {
+        "name" : "kani",
+        "age" : "27"
+    },
+
+];
+for(var i in arr) {
+    
+    console.log(arr[i].name);
+
+}
+
+
+
+
